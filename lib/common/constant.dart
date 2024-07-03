5f47766fcef59ccd3c2c0816ca5800d10672ca5b
muazzam <muazzamafaque@gmail.com>
2024-07-04 00:00:38 +0500

    all set

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,4 +1,2 @@
 const noConnection = 'Internet connection';
 const timeout = 'Connection timed out';
-const noConnectionMessage =
-    'You are currently offline, turn on your mobile data or wifi to connect.';
